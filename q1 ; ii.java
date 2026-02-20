7eb2400be7d3a8b548ec18da632f37c01209a0c0
Barbiescodes <barbiebabranesh@gmail.com>
2026-02-20 22:26:28 +0300

    Initial commit

---
+++ q1 ; ii.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class StudentApp {
+
+    public static void main(String[] args) {
+
+       
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter Student ID: ");
+        String studentID = scanner.nextLine();
+
+        System.out.print("Enter Name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter Course: ");
+        String course = scanner.nextLine();
+
+        
+        StudentRecord student = new StudentRecord(studentID, name, course);
+
+        
+        System.out.println("\nStudent Details:");
+        student.displayInfo();
+
+        
+        scanner.close();
+    }
+}
